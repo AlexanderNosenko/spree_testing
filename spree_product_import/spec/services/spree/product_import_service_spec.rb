69c030dce4d210ae1f47407da18cc02a5519794d
alexandrnosenko <sania.ace@gmail.com>
2019-07-07 15:06:58 +0200

    Tests for ProductImportService

--- spree_product_import/spec/services/spree/product_import_service_spec.rb
+++ spree_product_import/spec/services/spree/product_import_service_spec.rb
@@ -1,7 +1,29 @@
 describe Spree::ProductImportService do
+  stub_paperclip(Spree::ProductImportService)
+  IMPORT_FILE_ENTRIES_NO = 1
+
   describe '#call' do
+
+    let(:product_import) { create(:product_import) }
+    let(:product_import_file) do
+      file = File.open("#{Dir.pwd}/spec/services/spree/product_import_sample.csv")
+
+      {
+        file: file,
+        valid_entrires_no: 3
+      }
+    end
+
     context 'without all valid entries' do
-      it 'should create all valid products from data_file'
+      it 'should create all valid products from data_file' do
+        products_no_before = Spree::ProductImport.count
+
+        service = described_class.new(product_import.id, product_import_file[:file])
+        service_response = service.call
+
+        expect(service_response[:status]).to eq :success
+        expect(Spree::ProductImport.count).to eq products_no_before + product_import_file[:valid_entrires_no]
+      end
     end
 
     context 'with invalid entries' do
