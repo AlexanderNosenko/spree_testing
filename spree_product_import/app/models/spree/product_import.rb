3ac412f29e8738a093b03f19adfd51b2b6b90c61
alexandrnosenko <sania.ace@gmail.com>
2019-07-08 15:00:06 +0200

    Add verifying double to Spree::ProductImport spec

--- spree_product_import/app/models/spree/product_import.rb
+++ spree_product_import/app/models/spree/product_import.rb
@@ -16,17 +16,21 @@ module Spree
       failed: 3,
     }
 
-    after_commit :start_processing, on: :create, unless: -> { ship_processing }
+    after_commit :start_processing, on: :create, unless: -> { ship_processing || job_id.present?}
     def start_processing
       job_id = Spree::ProductImportJob.perform_later(id)
       update(job_id: job_id)
     end
 
     def process_data_file
-      service = Spree::ProductImportService.new(self)
+      service = Spree::ProductImportService.new(self, local_data_file, headers: true, col_seo: ';')
       service_result = service.call
 
       service_result[:status] == :success
+    end
+
+    def local_data_file
+      Paperclip.io_adapters.for(data_file).read
     end
 
   end
--- spree_product_import/spec/models/spree/product_import_spec.rb
+++ spree_product_import/spec/models/spree/product_import_spec.rb
@@ -1,12 +1,11 @@
 describe Spree::ProductImport do
   stub_paperclip(Spree::ProductImport)
 
+  before(:each) do
+    allow(Spree::ProductImportJob).to receive(:perform_later).and_return('job_id')
+  end
+
   describe '#create' do
-
-    before(:each) do
-      allow(Spree::ProductImportJob).to receive(:perform_later).and_return('job_id')
-    end
-
     it 'should create a record with :uploaded status' do
       product_import = described_class.create
 
@@ -22,10 +21,20 @@ describe Spree::ProductImport do
 
   describe '#process_data_file' do
     it 'should return true when service succeeds' do
+      service_class_double = class_double('Spree::ProductImportService')
+                               .as_stubbed_const(transfer_nested_constants: true)
       service_double = double(call: { status: :success })
-      allow(Spree::ProductImportService).to receive(:new).and_return(service_double)
+      allow(service_class_double).to receive(:new).and_return(service_double)
 
-      expect(create(:product_import).process_data_file).to eq(true)
+      file_double = double
+      allow_any_instance_of(Spree::ProductImport).to receive(:local_data_file).and_return(file_double)
+      product_import = create(:product_import)
+
+      result = product_import.process_data_file
+
+      expect(result).to eq(true)
+      expect(service_class_double).to have_received(:new).with(product_import, file_double, instance_of(Hash))
+      expect(service_double).to have_received(:call)
     end
   end
 end
